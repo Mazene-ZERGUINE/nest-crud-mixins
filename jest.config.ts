033e87bc27f24d4ce57c73ef961033903a41a8ee
Mazene <mmazenezerguine@gmail.com>
2025-03-02 20:31:55 +0100

    setting up jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    rootDir: 'tests',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverage: true,
+    coverageDirectory: '../coverage',
+};
